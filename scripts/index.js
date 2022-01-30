fac315ef88f75b55773a01158b4fb5eb99080db3
Svetlana A. Pivovarova <pivovarova.svetlana@gmail.com>
2022-01-30 19:41:48 +0400

    написать скрипт создания карточек из массива и пользователем через форму

--- scripts/index.js
+++ scripts/index.js
@@ -25,40 +25,89 @@ const initialCards = [
     }
   ];
 const buttonEditProfile = document.querySelector('.profile__edit-button');
-const buttonCloseProfile = document.querySelector('.popup__close-btn');
+const buttonCloseProfile = document.querySelector('.popup__close-btn_type_profile');
 const popup = document.querySelector('.popup');
 const popupProfile = document.querySelector('.popup_type_profile')
 const popupCard = document.querySelector('.popup_type_card');
 const popupOpenedClass = 'popup_opened';
-
 let formElement = document.querySelector('.form'); 
+let formProfile = document.querySelector('[name="profile-information"]');
 let nameInput = formElement.querySelector('.form__text_type_name'); 
 let jobInput = formElement.querySelector('.form__text_type_about');
-
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession'); 
-
+const cardsWrap = document.querySelector('.elements');    // обертка для карточек
+const formNewPlace = document.querySelector('[name="new-place-card"]'); //выбрать форму добавления нового места
+let cardInputTitle = document.querySelector('.form__text_type_place-title'); //выбрать поле Название
+let cardInputLink = document.querySelector('.form__text_type_place-link');    //выбрать поле ссылка
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card'); //выбрать элемент темплейта для создания карточки
 const buttonAddCard = document.querySelector('.profile__add-button');
+const buttonCloseAddCard = document.querySelector('.popup__close-btn_type_card');
 
 
-function openPopup(item) {
+function openPopup(item) {  //функция открытия поп-ап профила
     item.classList.add(popupOpenedClass);
-    nameInput.value = profileName.textContent;
+    nameInput.value = profileName.textContent;  
     jobInput.value = profileProfession.textContent;
 };
 
-function closePopup(item) {
+function closePopup(item) {     //функция закрытия поп-ап
     item.classList.remove(popupOpenedClass);
 };
 
-function formSubmitHandler (evt, item) {
+function formSubmitUserHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileProfession.textContent = jobInput.value;
-    closePopup(item);
+    closePopup(popupProfile);
 };
 
-buttonEditProfile.addEventListener ('click', function() {
+const handleLikeButton = (e) => {
+  e.target.classList.add('card__like-icon_active');
+}
+
+const handleDeleteButton = (e) => {
+  e.target.closest('.card').remove();
+}
+
+const getCardElement = (item) => {                            //получить элемент для карточки
+  const card = cardTemplate.cloneNode(true);
+  const cardTitle = card.querySelector('.card__place-title');
+  const cardImage = card.querySelector('.card__image');
+  const cardLikeButton = card.querySelector('.card__like-icon');
+  const cardDeleteButton = card.querySelector('.card__delete-icon');
+
+  cardLikeButton.addEventListener('click', handleLikeButton);
+  cardDeleteButton.addEventListener('click', handleDeleteButton);
+  cardTitle.textContent = item.name;     //наполнить содержимым - название
+  cardImage.src = item.link;             //наполнить содержимым - ссылка
+  cardImage.alt = item.name;             //наполнить содержимым - alt
+  
+  return card;
+}
+
+const renderCard = (item, wrap) => {
+  const card = getCardElement(item)
+  wrap.prepend(card)
+}
+
+const handleCardFormSubmit = (evt) => {   //форма для создания новой карточки пользователем
+  evt.preventDefault();
+  const cardUser = {
+      name: cardInputTitle.value,
+      link: cardInputLink.value
+  };
+  renderCard(cardUser, cardsWrap);          //вызвать функцию создания карточки, передать переменные card из формы и cardsWrap
+  
+  closePopup(popupCard);
+  formNewPlace.reset();
+};
+
+initialCards.forEach(item => {          //создать 6 первоначальных карточек, данные хранятся в массиве initialCards
+  renderCard(item, cardsWrap)
+});
+
+buttonEditProfile.addEventListener ('click', function() {  //слушатель для кнопки "редактировать профиль"
     openPopup(popupProfile);
 });
 
@@ -70,6 +119,10 @@ buttonAddCard.addEventListener ('click',function() {
     openPopup(popupCard);
 });
 
-formElement.addEventListener('submit', formSubmitHandler); 
+buttonCloseAddCard.addEventListener ('click', function() {
+  closePopup(popupCard);
+})
 
-element.addEventListener('event', () => { yourFunction(functionArgument) })
+formProfile.addEventListener('submit', formSubmitUserHandler); 
+
+formNewPlace.addEventListener('submit', handleCardFormSubmit);
