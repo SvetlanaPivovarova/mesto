90b17b2e6b7a68319d19903d1d5e420de51a80df
Svetlana A. Pivovarova <pivovarova.svetlana@gmail.com>
2022-03-21 08:36:30 +0400

    удалить комментарии из PopupWithImage

--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -3,21 +3,11 @@ import {Popup} from "./Popup.js";
 export class PopupWithImage extends Popup {
     static selectors = {
         popupSelector: '.popup_type_image',
-        //formNewPlace: '[name="new-place-card"]',
-        //imageSelector: '.popup__image-item',
-        //captionSelector: '.popup__title_type_image-caption'
     }
     constructor() {
         super(PopupWithImage.selectors.popupSelector);
-        //this._previewImage = document.querySelector(PopupWithImage.selectors.imageSelector);
-        //this._previewCaption = document.querySelector(PopupWithImage.selectors.captionSelector);
-        //this._handleCardClick = handleCardClick;
     }
     open() {
         super.open();
-        //this._previewImage.src = link;
-        //this._previewCaption.textContent = name;
-        //this._previewImage.alt = name;
-        //this._handleCardClick(); //?
     }
 }
