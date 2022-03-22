86fbf4ec4e093a7068a0d67769e6e0da0aa84dc0
Svetlana A. Pivovarova <pivovarova.svetlana@gmail.com>
2022-03-22 09:14:09 +0400

    дописать метод _getInputValues, который собирает данные всех полей формы, применить к форме создания карточки

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -5,23 +5,36 @@ export class PopupWithForm extends Popup {
         formSelector: '.form',
         inputSelector: '.form__text',
     }
-    constructor(popupSelector, handleCardFormSubmit) {
+    constructor({ popupSelector, handleFormSubmit }) {
         super(popupSelector);
-        this._handleCardFormSubmit = handleCardFormSubmit;
+        this._handleFormSubmit = handleFormSubmit;
         this._form = this._popupElement.querySelector(PopupWithForm.selectors.formSelector);
     }
     _getInputValues() {
-        const inputValues = {};
-        this._inputs = [...this._form.querySelectorAll(PopupWithForm.selectors.inputSelector)];
-        this._inputs.forEach((inputElement) => {
-            const inputName = inputElement.getAttribute('name');
-            inputValues[inputName] = inputElement.value;
-        });
-        return inputValues;
+        //this._inputValues = {};
+        //this._inputs = [...this._form.querySelectorAll(PopupWithForm.selectors.inputSelector)];
+        //this._inputs.forEach((inputElement) => {
+        //    const inputName = inputElement.getAttribute('name');
+        //    this._inputValues[inputName] = inputElement.value;
+        //});
+        this._inputList = this._form.querySelectorAll(PopupWithForm.selectors.inputSelector);
+
+        this._inputValues = {};
+        this._inputList.forEach(input => this._inputValues[input.name] = input.value);
+        console.log(this._inputValues);
+        return this._inputValues;
+
+        //return this._inputValues;
     }
     setEventListeners() {
         super.setEventListeners();
-        this._form.addEventListener('submit', this._handleCardFormSubmit);
+        this._form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+
+            // добавим вызов функции _handleFormSubmit
+            // передадим ей объект — результат работы _getInputValues
+            this._handleFormSubmit(this._getInputValues());
+        });
     }
     close() {
         super.close();
--- pages/index.js
+++ pages/index.js
@@ -60,26 +60,26 @@ function handleProfileFormSubmit (evt) {
 };
 
 //форма для создания новой карточки пользователем
-const handleCardFormSubmit = (evt) => {   
-  evt.preventDefault();
+//const handleCardFormSubmit = (evt) => {
+ // evt.preventDefault();
 
-  const cardUser = [{
-      name: cardInputTitle.value,
-      link: cardInputLink.value
-  }];
-  const cardUserList = new Section({
-      data: cardUser,
-      renderer:  (item) =>{
-          const card = new Card(item, '.card-template-default', handleCardClick);
-          const cardElement = card.generateCard();
-          cardUserList.addItem(cardElement);
-      }
-  }, '.elements'
-  );
-
-    cardUserList.renderItems();
-    popupCard.close();
-};
+ // const cardUser = [{
+ //     name: cardInputTitle.value,
+ //     link: cardInputLink.value
+ // }];
+ // const cardUserList = new Section({
+   //   data: cardUser,
+   //   renderer:  (item) =>{
+    //      const card = new Card(item, '.card-template-default', handleCardClick);
+    //      const cardElement = card.generateCard();
+    //      cardUserList.addItem(cardElement);
+    //  }
+  //}, '.elements'
+  //);
+//
+   // cardUserList.renderItems();
+   // popupCard.close();
+//};
 
 //функция открывания попап с картинкой при клике на карточку
 function handleCardClick(name, link) {
@@ -90,9 +90,30 @@ function handleCardClick(name, link) {
 }
 
 //создать для каждого попапа свой экземпляр класса PopupWithForm
-const popupProfile = new PopupWithForm('.popup_type_profile', handleProfileFormSubmit);
+const popupProfile = new PopupWithForm({
+    popupSelector: '.popup_type_profile',
+    handleFormSubmit: handleProfileFormSubmit
+});
 
-const popupCard = new PopupWithForm('.popup_type_card', handleCardFormSubmit);
+const popupCard = new PopupWithForm({
+    popupSelector: '.popup_type_card',
+    handleFormSubmit:  (cardUser) => {
+        const cardUserList = new Section({
+                data: [cardUser],
+                renderer:  (item) =>{
+                    const card = new Card(item, '.card-template-default', handleCardClick);
+                    const cardElement = card.generateCard();
+                    cardUserList.addItem(cardElement);
+                }
+            }, '.elements'
+        );
+
+        cardUserList.renderItems();
+        popupCard.close();
+    }
+});
+
+console.log(popupCard._getInputValues());
 
 const popupFullSizeImage = new PopupWithImage(handleCardClick);
 
